f4aa346466bb84c2fb0ec3d7055fd8ff96cc4e0c
maxingyuan <mxydl2009@163.com>
2024-07-06 15:50:39 +0800

    添加构建配置的注释

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,12 @@
+// 压缩代码，删除注释
 import terser from "@rollup/plugin-terser";
+// 解析第三方模块
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+// 解析CommonJS规范的代码
 import commonjs from "@rollup/plugin-commonjs";
+// 模块banner信息
 import { bannerInfo } from "./banner.cjs";
+// babel插件，转译代码
 import babel from "@rollup/plugin-babel";
 
 export default {
